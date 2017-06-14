04c8c224dacbe08be60f88cea2af7012fad2c2ba
ARATAYOKOYAMA <bob2525@hotmail.co.jp>
2017-06-14 17:58:45 +0900

    横山　じぷり内で使われているNCMBのkeyをdummy用(横山個人のNCMB)に変更しました。今後、dummy用のNCMBはチームで作成してください。

--- www/js/common.js
+++ www/js/common.js
@@ -169,7 +169,8 @@ function ExportList(eventObj, participants) {
             });
     // Safariやchromeを用いて、生成したcsvを表示
     //window.open('https://mb.api.cloud.nifty.com/2013-09-01/applications/zMCtGPFYQE1JZ2UY/publicFiles/' + filename +'','_system','toolbarposition=top,suppressesIncrementalRendering=yes,location=no,closebuttoncaption=戻る');
-    window.open('https://mb.api.cloud.nifty.com/2013-09-01/applications/zMCtGPFYQE1JZ2UY/publicFiles/' + filename +'','_system','location=no');
+    //window.open('https://mb.api.cloud.nifty.com/2013-09-01/applications/zMCtGPFYQE1JZ2UY/publicFiles/' + filename +'','_system','location=no');
+    window.open('https://mb.api.cloud.nifty.com/2013-09-01/applications/9pPO2A36w5QxNyvG/publicFiles/' + filename +'','_system','location=no'); // デバッグ
 }
 
 function open_jinkawaHP(){
